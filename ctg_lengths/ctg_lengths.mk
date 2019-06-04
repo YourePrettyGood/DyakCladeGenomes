1bda8736755028fbf05a9d1af23de521501ca583
Patrick Reilly <pegasusflightresources@gmail.com>
2019-06-04 11:59:27 -0400

    Added assembly contiguity plotting makefile, R script, and plot.

---
+++ ctg_lengths/ctg_lengths.mk
@@ -0,0 +1,67 @@
+SHELL=/bin/bash
+
+#Generate length-ordered contig length distributions for
+# all assemblies, and compare to Dmel scaffold lengths.
+#Plot generated at the end is a cumulative contig length
+# distribution.
+#Assumes refs are available in subdirectory of parent directory
+
+#Expects the following in your PATH:
+#fasta_formatter
+#AssemblyContiguityPlot.R
+#This R script is available in the tools subdirectory
+# of the Github repository.
+
+#Options to adjust:
+#Ref to use scaffolds from as ideal:
+IDEALREF := Dmel_ISO1
+#Final plot filename:
+PLOTNAME := MSTYY_ctg_len_dist.pdf
+
+#DO NOT CHANGE THE BELOW:
+#References inferred from directory above the current:
+REFS := $(basename $(notdir $(wildcard ../refs/*.fasta)))
+#Old reference(s) (not PacBio one(s)), inferred from "old" subdirectory of
+# "refs" directory above current:
+OLDREFS := $(basename $(notdir $(wildcard ../refs/old/*.fasta)))
+#Contig length files:
+CTGLENS := $(addsuffix _ctglens.tsv,$(REFS))
+#Scaffold length file:
+SCAFLEN := $(addsuffix _scaflens.tsv,$(IDEALREF))
+#Contig length file(s) for old references (not our PacBio ones):
+EXTRACTGLENS := $(addsuffix _ctglens.tsv,$(OLDREFS))
+
+.PHONY : all clean usage
+
+.SECONDARY : $(CTGLENS) $(SCAFLEN) $(EXTRACTGLENS) combined_lens.tsv
+
+usage :
+	@echo "Usage:"
+	@echo "make -f ctg_lengths.mk [task]"
+	@echo "Tasks:"
+	@echo "all -> Provide length-sorted lists of contig lengths (plus scaffold lengths for an ideal genome)"
+	@echo "clean -> Clean up all output files and directories"
+
+all : $(PLOTNAME)
+
+#Generate the cumulative contig length plot:
+$(PLOTNAME) : combined_lens.tsv
+	AssemblyContiguityPlot.R $@ $< Ideal_$(IDEALREF) $(REFS) $(OLDREFS)
+
+#Combine the contig and scaffold length files:
+combined_lens.tsv : $(CTGLENS) $(SCAFLEN) $(EXTRACTGLENS)
+	cat $^ > $@
+
+$(EXTRACTGLENS) : %_ctglens.tsv : ../refs/old/%.fasta
+	fasta_formatter -i $< | awk '/^>/{header=$$0;}!/^>/{split($$0, ctgs, /[N]+/); for (ctg in ctgs) {print header"_ctg"ctg; print ctgs[ctg];};}' | awk '!/^>/{print length($$0);}' | sort -k1,1nr | awk '{print "$*\t"NR"\t"$$1;}' > $@
+
+#Sort scaffold lengths for the ideal reference:
+$(SCAFLEN) : ../refs/$(IDEALREF).fasta
+	fasta_formatter -i $< | awk '!/^>/{print length($$0);}' | sort -k1,1nr | awk '{print "Ideal_$(IDEALREF)\t"NR"\t"$$1;}' > $@
+
+#Sort contig lengths for the other references:
+$(CTGLENS) : %_ctglens.tsv : ../refs/%.fasta
+	fasta_formatter -i $< | awk '/^>/{header=$$0;}!/^>/{split($$0, ctgs, /[N]+/); for (ctg in ctgs) {print header"_ctg"ctg; print ctgs[ctg];};}' | awk '!/^>/{print length($$0);}' | sort -k1,1nr | awk '{print "$*\t"NR"\t"$$1;}' > $@
+
+clean :
+	rm -f $(CTGLENS) $(SCAFLEN) $(EXTRACTGLENS) combined_lens.tsv $(PLOTNAME)
---
+++ tools/AssemblyContiguityPlot.R
@@ -0,0 +1,50 @@
+#!/usr/bin/env Rscript
+
+options <- commandArgs(trailingOnly=TRUE)
+
+check_package <- function(pkg_name) {
+   if (!require(pkg_name, character.only=TRUE)) {
+      install.packages(pkg_name, dependencies=TRUE, type="source", repos="http://cran.us.r-project.org")
+   }
+   library(pkg_name, character.only=TRUE)
+}
+
+check_package("tidyverse")
+
+#Read in arguments:
+plot_file <- options[1]
+input_file <- options[2]
+asm_names <- options[-c(1,2)]
+
+#Plotting details:
+colorblind_palette <- c("#009E73", "#E79F00", "#9AD0F3", "#0072B2", "#D55E00", "#CC79A7", "#F0E442", "#000000")
+
+all_lens <- read.table(input_file, colClasses=c("character", "integer", "integer"), col.names=c("Assembly", "Index", "Length"), header=FALSE)
+
+Assembly_levels <- asm_names
+Assembly_labels <- gsub("_", " ", asm_names, fixed=TRUE)
+#Assembly_levels <- c("Dmel_ISO1_FBr6.26_scafs", "Dmel_ISO1_FBr6.26", "Dsan_STOCAGO1482", "Dtei_GT53w", "Dyak_NY73PB", "Dyak_Tai18E2", "Dyak_Tai18E2_FBr1.05")
+#Assembly_labels <- c("Ideal Dmel ISO1", "Dmel ISO1", "Dsan STO CAGO 1482", "Dtei GT53w", "Dyak NY73PB", "Dyak Tai18E2", "Dyak Tai18E2 FlyBase")
+contiguity_plot <- all_lens %>% mutate(Assembly=factor(Assembly, levels=Assembly_levels, labels=Assembly_labels)) %>%
+  group_by(Assembly) %>%
+  mutate(CumulativeLength=cumsum(Length)) %>%
+  ggplot(aes(x=Index, y=CumulativeLength, colour=Assembly)) +
+    geom_line(size=1) +
+    scale_x_log10() +
+    scale_colour_manual(values=colorblind_palette) +
+    theme_bw() +
+    theme(axis.line = element_line(colour = "black"),
+      panel.border=element_rect(colour="black", size=1.5),
+      aspect.ratio=0.4,
+      text=element_text(size=12),
+      legend.text=element_text(size=8),
+      legend.title=element_text(face="bold", size=12),
+      axis.title.y=element_text(vjust=1, face="bold", size=12, margin=margin(10,20,10,0)),
+      axis.title.x=element_text(vjust=1, margin=margin(10,20,10,0), face="bold", size=12),
+      axis.text.x=element_text(size=11),
+      axis.text.y=element_text(size=11),
+      plot.title=element_text(face="bold")) +
+  labs(x=expression(paste("Contig Rank (", log[10], " scale)")),
+    y="Cumulative Assembly Length (bp)",
+    title="Assembly Contiguity")
+ggsave(plot_file, plot=contiguity_plot, dpi=500, units="cm", width=16.0, height=12.0)
